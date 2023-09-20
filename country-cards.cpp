cdd12f5dce1cea0b10e21c640ca93d09a8242897
Paweł Walenda <pawel.walenda@gmail.com>
2023-09-20 21:57:07 +0200

    Add country-cards

---
+++ country-cards.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string country, capital;
+
+    //your code goes here
+    cin >> country;
+    cin >> capital;
+    cout << "Country: " << country << endl;
+    cout << "Capital: " << capital;
+}
