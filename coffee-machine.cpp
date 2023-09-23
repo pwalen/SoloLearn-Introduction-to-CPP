a972c9d0545e057fd79df5a060e97725313dc77a
Paweł Walenda <pawel.walenda@gmail.com>
2023-09-23 12:56:55 +0200

    Add coffee-machine

---
+++ coffee-machine.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int choice;
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        cout << "Espresso";
+        break;
+    case 2:
+        cout << "Americano";
+        break;
+    case 3:
+        cout << "Cappuccino";
+        break;
+    case 4:
+        cout << "Latte";
+        break;
+    default:
+        cout << "invalid";
+    }
+}
